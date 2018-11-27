70cf6dd898c171ab24a5df0edbed6234db93a5c7
cfeim <cfeimster@live.com>
2018-11-26 22:58:15 -0500

    More progress

--- app/src/main/java/com/example/cfeim/tangerine/MainActivity.java
+++ app/src/main/java/com/example/cfeim/tangerine/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,14 +14,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static TextView edit_item;
-    public static TextView edit_zipcode;
-    public static Spinner spinner_radius;
-    public static Button button_go;
+    public TextView edit_item;
+    public TextView edit_zipcode;
+    public Spinner spinner_radius;
+    public Button button_go;
 
     private static String string_item = "";
     private static String string_zipcode = "";
-    private static String string_radius = "";
+    private static Integer int_radius = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +33,21 @@ public class MainActivity extends AppCompatActivity {
         spinner_radius = findViewById(R.id.spinner_radius);
         button_go = findViewById(R.id.button_go);
 
+        String[] strArr = getResources().getStringArray(R.array.radius);
+        Integer[] intArr = new Integer[strArr.length];
+        for (int i = 0; i < intArr.length; i++){
+            intArr[i] = Integer.parseInt(strArr[i]);
+        }
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, intArr);
+        spinner_radius.setAdapter(adapter);
+
         button_go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Log.d("button", "go button was pressed");
                 string_item = edit_item.getText().toString();
                 string_zipcode = edit_zipcode.getText().toString();
-                string_radius = spinner_radius.getSelectedItem().toString();
+                int_radius = (Integer)spinner_radius.getSelectedItem();
 
                 if(TextUtils.isEmpty(string_item)){
                     Toast.makeText(MainActivity.this, "You must enter an item to search.", Toast.LENGTH_SHORT).show();
@@ -56,7 +66,14 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                    Log.d("a1", "string_search : " + string_item);
+                    Log.d("a1", "string_zipcode : " + string_zipcode);
+                    Log.d("a1", "int_radius : " + String.valueOf(int_radius));
+                    Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                    i.putExtra("item_search", string_item);
+                    i.putExtra("item_zipcode", string_zipcode);
+                    i.putExtra("item_radius", int_radius);
+                    startActivity(i);
                 }
             }
         });
--- app/src/main/java/com/example/cfeim/tangerine/SecondActivity.java
+++ app/src/main/java/com/example/cfeim/tangerine/SecondActivity.java
@@ -2,12 +2,30 @@ package com.example.cfeim.tangerine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private String item;
+    private String zipcode;
+    private int radius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            item = extras.getString("item_search");
+            zipcode = extras.getString("item_zipcode");
+            radius = extras.getInt("item_radius");
+
+            Log.d("a2", "item : " + item);
+            Log.d("a2", "zipcode : " + zipcode);
+            Log.d("a2", "radius : " + String.valueOf(radius));
+        }
+
     }
 }
