68a9576cec6689cdc2135ac949e26f6c2d2ab5aa
cfeim <cfeimster@live.com>
2018-11-28 21:49:19 -0500

    Laptop isnt happy

--- app/src/main/java/com/example/cfeim/tangerine/SecondActivity.java
+++ app/src/main/java/com/example/cfeim/tangerine/SecondActivity.java
@@ -4,11 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private String apiKey = "Po9k2i9YAbWEjU5Kp0ey6J4ImKSKKAnrWVpXhRyPkt0CesMGE2Sw5TfATIwJ5OzF";
 
     private String item;
     private String zipcode;
     private int radius;
+
+    private String city;
+    private ArrayList<String> zips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,41 @@ public class SecondActivity extends AppCompatActivity {
             Log.d("a2", "item : " + item);
             Log.d("a2", "zipcode : " + zipcode);
             Log.d("a2", "radius : " + String.valueOf(radius));
+        }
+
+        try
+        {
+            zips = new ZipCodeTask().execute(
+                    "https://www.zipcodeapi.com/rest/" +
+                            apiKey +
+                            "/radius.json/" +
+                            zipcode +
+                            "/" +
+                            String.valueOf(1) +
+                            "/mile")
+                    .get(5000, TimeUnit.MILLISECONDS);
+        } catch (java.lang.InterruptedException | java.util.concurrent.ExecutionException | java.util.concurrent.TimeoutException e)
+        {
+            Log.d("e", e.toString());
+        }
+
+        city = zips.get(0);
+        Log.d("a2", "city in array list : " + city);
+
+        try
+        {
+            zips = new ZipCodeTask().execute(
+                    "https://www.zipcodeapi.com/rest/" +
+                            apiKey +
+                            "/radius.json/" +
+                            zipcode +
+                            "/" +
+                            String.valueOf(radius) +
+                            "/mile")
+                    .get(5000, TimeUnit.MILLISECONDS);
+        } catch (java.lang.InterruptedException | java.util.concurrent.ExecutionException | java.util.concurrent.TimeoutException e)
+        {
+            Log.d("e", e.toString());
         }
 
     }
