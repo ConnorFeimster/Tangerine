5f56a4a521f6948d59768414cbf15b03860d74e0
cfeim <cfeimster@live.com>
2018-12-03 14:08:57 -0500

    CraigslistItem almost done

--- app/src/main/java/com/example/cfeim/tangerine/CraigsListTask.java
+++ app/src/main/java/com/example/cfeim/tangerine/CraigsListTask.java
@@ -4,10 +4,26 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class CraigsListTask extends AsyncTask<String, Void, ArrayList<CraigslistItem>> {
 
@@ -17,6 +33,25 @@ public class CraigsListTask extends AsyncTask<String, Void, ArrayList<Craigslist
         HttpURLConnection connection = null;
         ArrayList<CraigslistItem> result = new ArrayList<>();
 
+        String money = "&#x0024;";
+        String title;
+        String[] split;
+
+        String itemTitle;
+        String itemURL;
+        String itemDesc;
+        int itemPrice;
+        boolean itemHide;
+
+        Node node;
+        Element element1;
+        NodeList titleList;
+        Element titleElement;
+        NodeList urlList;
+        Element urlElement;
+        NodeList descList;
+        Element descElement;
+
         try
         {
             URL url = new URL(params[0]);
@@ -24,8 +59,42 @@ public class CraigsListTask extends AsyncTask<String, Void, ArrayList<Craigslist
             connection.connect();
             if(connection.getResponseCode() == HttpURLConnection.HTTP_OK)
             {
-                String xml = IOUtils.toString(connection.getInputStream(), "UTF8");
-                Log.d("xml", "xml : " + xml);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+
+                NodeList nodeList = doc.getElementsByTagName("item");
+
+                Log.d("list", "NodeList length : " + nodeList.getLength());
+
+                node = nodeList.item(0);
+                element1 = (Element) node;
+
+                titleList = element1.getElementsByTagName("title");
+                titleElement = (Element) titleList.item(0);
+                titleList = titleElement.getChildNodes();
+                title = titleList.item(0).getNodeValue();
+
+                urlList = element1.getElementsByTagName("link");
+                urlElement = (Element) urlList.item(0);
+                urlList = urlElement.getChildNodes();
+                itemURL = urlList.item(0).getNodeValue();
+
+                descList = element1.getElementsByTagName("description");
+                descElement = (Element) descList.item(0);
+                descList = descElement.getChildNodes();
+                itemDesc = descList.item(0).getNodeValue();
+
+                split = title.split(money);
+                itemTitle = split[0];
+                itemPrice = Integer.parseInt(split[1]);
+
+
+                Log.d("i", "title : " + itemTitle);
+                Log.d("i", "url : " + itemURL);
+                Log.d("i", "desc : " + itemDesc);
+                Log.d("i", "price : " + itemPrice);
             }
             else
             {
